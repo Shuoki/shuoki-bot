67880ebef6915af7614ee3e9f9a65859579afd3b
TheMisterSenpai <kirasimshow@gmail.com>
2021-11-05 18:34:36 +0300

    да

--- index.js
+++ index.js
@@ -1,17 +1,33 @@
-const Discord = require('discord.js')
+const {Client, Intents, Collection} = require('discord.js')
 const Dokdo = require('dokdo')
+const fs = require('fs')
 const { prefix, token } = require('./config.json')
 
-const client = new Discord.Client({ intents: ['GUILDS', 'GUILD_MESSAGES'] })
-const DokdoHandler = new Dokdo(client, { aliases: ['dokdo', 'dok'], prefix: `${prefix}` })
+const client = new Client({
+    intents: ['GUILDS', 'GUILD_MESSAGES'],
+    disableMention: 'everyone'
+})
+const DokdoHandler = new Dokdo(client, {
+    aliases: ['dokdo', 'dok'],
+    prefix: `${prefix}`
+})
 
 client.once('ready', () => {
-    console.log('Ready!');
+    console.log(`Shuoki#0912 запустилась на ${client.guilds.cache.size} серверах`)
+    client.user.setActivity(`League of legends`, {type: 0})
 })
 
 client.on('messageCreate', async message => {
-    if (message.content === 'ping') return message.channel.send('Pong') // handle commands first
-    DokdoHandler.run(message) // try !dokdo
+    DokdoHandler.run(message)
 })
+
+client.commands = new Collection();
+const commandFiles = fs.readdirSync('./command').filter(file => file.endsWith('.js'))
+
+for (const file of commandFiles) {
+    const command = require(`./command/${file}`);
+    console.log(`${file} был загружен`)
+    client.commands.set(command.name, command);
+}
 
 client.login(token)
